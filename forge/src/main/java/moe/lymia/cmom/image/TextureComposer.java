2bdf2d54923a1c31cac2752e058ac56cdac06c70
Alissa Rao <lymia@lymia.moe>
2024-01-28 04:38:55 -0800

    Committing early work on TextureComposer

--- forge/src/main/java/moe/lymia/cmom/image/TextureComposer.java
+++ forge/src/main/java/moe/lymia/cmom/image/TextureComposer.java
@@ -1,9 +1,11 @@
 package moe.lymia.cmom.image;
 
+import moe.lymia.cmom.contrib.swingx.BlendComposite;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class TextureComposer {
+public final class TextureComposer {
     private final BufferedImage image;
     private final Graphics2D gfx;
 
@@ -13,10 +15,21 @@ public class TextureComposer {
     }
 
     public void composeAlpha(BufferedImage src) {
-        image.getGraphics().drawImage(src, 0, 0, image.getWidth(), image.getHeight(), null);
+        gfx.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+        gfx.drawImage(src, 0, 0, image.getWidth(), image.getHeight(), null);
     }
 
-    public void composeSoftLight(BufferedImage src) {
+    private static BufferedImage layerBlend(BufferedImage src, float alpha) {
+        BufferedImage dst = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics2D gfx = dst.createGraphics();
+        gfx.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+        gfx.drawImage(src, 0, 0, null);
+        return dst;
+    }
 
+    public void composeEffect(BufferedImage src, float alpha, BlendComposite effect) {
+        BufferedImage layer = layerBlend(src, alpha);
+        gfx.setComposite(effect);
+        gfx.drawImage(layer, 0, 0, image.getWidth(), image.getHeight(), null);
     }
 }
