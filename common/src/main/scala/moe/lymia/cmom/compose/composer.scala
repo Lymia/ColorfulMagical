c389158ecacbe101567dd78a88eee908ad0cc6bc
Alissa Rao <lymia@lymia.moe>
2024-01-29 09:49:25 -0800

    Finish implementing blending modes.

--- common/src/main/scala/moe/lymia/cmom/compose/composer.scala
+++ common/src/main/scala/moe/lymia/cmom/compose/composer.scala
@@ -1,6 +1,6 @@
-package moe.lymia.cmom.image
+package moe.lymia.cmom.compose
 
-import moe.lymia.cmom.image.TextureSource.SpriteSheetLocation
+import moe.lymia.cmom.compose.TextureSource.SpriteSheetLocation
 import org.jdesktop.swingx.graphics.BlendComposite
 
 import java.awt.*
@@ -30,48 +30,59 @@ enum TextureSource {
 
 enum BlendingMode {
   case Alpha, Multiply, Screen, Difference, Addition, Subtract, DarkenOnly, LightenOnly
+  case OklabHue, OklabChroma, OklabColor, OklabLightness
+  case Dodge, Burn, HardLight, SoftLight, Overlay
 
   lazy val compositor: Composite = this match {
-    case Alpha       => AlphaComposite.getInstance(AlphaComposite.SRC_OVER)
-    case Multiply    => BlendComposite.Multiply
-    case Screen      => BlendComposite.Screen
-    case Difference  => BlendComposite.Difference
-    case Addition    => BlendComposite.Add
-    case Subtract    => BlendComposite.Subtract
-    case DarkenOnly  => BlendComposite.Darken
-    case LightenOnly => BlendComposite.Lighten
+    case Alpha          => AlphaComposite.getInstance(AlphaComposite.SRC_OVER)
+    case Multiply       => BlendComposite.Multiply
+    case Screen         => BlendComposite.Screen
+    case Difference     => BlendComposite.Difference
+    case Addition       => BlendComposite.Add
+    case Subtract       => BlendComposite.Subtract
+    case DarkenOnly     => BlendComposite.Darken
+    case LightenOnly    => BlendComposite.Lighten
+    case OklabHue       => GimpComposite(GimpBlendingMode.OklabHue)
+    case OklabChroma    => GimpComposite(GimpBlendingMode.OklabChroma)
+    case OklabColor     => GimpComposite(GimpBlendingMode.OklabColor)
+    case OklabLightness => GimpComposite(GimpBlendingMode.OklabLightness)
+    case Dodge          => BlendComposite.ColorDodge
+    case Burn           => BlendComposite.ColorBurn
+    case HardLight      => BlendComposite.HardLight
+    case SoftLight      => BlendComposite.SoftLight
+    case Overlay        => BlendComposite.Overlay
   }
 }
 object BlendingMode {
   private val GIMP_MODE_MAP = Map(
     "Normal (legacy)"             -> Some(BlendingMode.Alpha),
-    "Dissolve (legacy)"           -> None,
+    "Dissolve (legacy)"           -> Some(BlendingMode.Alpha),          // just do alpha; wrong, but close enough
     "Behind (legacy)"             -> None,
     "Multiply (legacy)"           -> Some(BlendingMode.Multiply),
     "Screen (legacy)"             -> Some(BlendingMode.Screen),
-    "Old broken Overlay"          -> None,
+    "Old broken Overlay"          -> Some(BlendingMode.Overlay),        // idc if this unbreaks it
     "Difference (legacy)"         -> Some(BlendingMode.Difference),
     "Addition (legacy)"           -> Some(BlendingMode.Addition),
     "Subtract (legacy)"           -> Some(BlendingMode.Subtract),
     "Darken only (legacy)"        -> Some(BlendingMode.LightenOnly),
     "Lighten only (legacy)"       -> Some(BlendingMode.DarkenOnly),
-    "Hue (HSV) (legacy)"          -> None,
-    "Saturation (HSV) (legacy)"   -> None,
-    "Color (HSL) (legacy)"        -> None,
-    "Value (HSV) (legacy)"        -> None,
+    "Hue (HSV) (legacy)"          -> Some(BlendingMode.OklabHue),       // TODO: HSV/HSL
+    "Saturation (HSV) (legacy)"   -> Some(BlendingMode.OklabChroma),    // TODO: HSV/HSL
+    "Color (HSL) (legacy)"        -> Some(BlendingMode.OklabColor),     // TODO: HSV/HSL
+    "Value (HSV) (legacy)"        -> Some(BlendingMode.OklabLightness), // TODO: HSV/HSL
     "Divide (legacy)"             -> None,
-    "Dodge (legacy)"              -> None,
-    "Burn (legacy)"               -> None,
-    "Hard Light (legacy)"         -> None,
-    "Soft light (legacy)"         -> None,
+    "Dodge (legacy)"              -> Some(BlendingMode.Dodge),
+    "Burn (legacy)"               -> Some(BlendingMode.Burn),
+    "Hard Light (legacy)"         -> Some(BlendingMode.HardLight),
+    "Soft light (legacy)"         -> Some(BlendingMode.SoftLight),
     "Grain extract (legacy)"      -> None,
     "Grain merge (legacy)"        -> None,
     "Color erase (legacy)"        -> None,
-    "Overlay"                     -> None,
-    "Hue (LCH)"                   -> None,
-    "Chroma (LCH)"                -> None,
-    "Color (LCH)"                 -> None,
-    "Lightness (LCH)"             -> None,
+    "Overlay"                     -> Some(BlendingMode.Overlay),
+    "Hue (LCH)"                   -> Some(BlendingMode.OklabHue),
+    "Chroma (LCH)"                -> Some(BlendingMode.OklabChroma),
+    "Color (LCH)"                 -> Some(BlendingMode.OklabColor),
+    "Lightness (LCH)"             -> Some(BlendingMode.OklabLightness),
     "Normal"                      -> Some(BlendingMode.Alpha),
     "Behind"                      -> None,
     "Multiply"                    -> Some(BlendingMode.Multiply),
@@ -81,15 +92,15 @@ object BlendingMode {
     "Substract"                   -> Some(BlendingMode.Subtract),
     "Darken only"                 -> Some(BlendingMode.DarkenOnly),
     "Lighten only"                -> Some(BlendingMode.LightenOnly),
-    "Hue (HSV)"                   -> None,
-    "Saturation (HSV)"            -> None,
-    "Color (HSL)"                 -> None,
-    "Value (HSV)"                 -> None,
+    "Hue (HSV)"                   -> Some(BlendingMode.OklabHue),       // TODO: HSV/HSL
+    "Saturation (HSV)"            -> Some(BlendingMode.OklabChroma),    // TODO: HSV/HSL
+    "Color (HSL)"                 -> Some(BlendingMode.OklabColor),     // TODO: HSV/HSL
+    "Value (HSV)"                 -> Some(BlendingMode.OklabLightness), // TODO: HSV/HSL
     "Divide"                      -> None,
-    "Dodge"                       -> None,
-    "Burn"                        -> None,
-    "Hard light"                  -> None,
-    "Soft light"                  -> None,
+    "Dodge"                       -> Some(BlendingMode.Dodge),
+    "Burn"                        -> Some(BlendingMode.Burn),
+    "Hard light"                  -> Some(BlendingMode.HardLight),
+    "Soft light"                  -> Some(BlendingMode.SoftLight),
     "Grain extract"               -> None,
     "Grain merge"                 -> None,
     "Vivid light"                 -> None,
